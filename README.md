acc331cb5ad37092662cfdc4a398cd0c72948fb2
Michael Salvagna <>
2024-05-01 14:56:28 -0400

    Added elevenLabs audio conversion

--- README.md
+++ README.md
@@ -2,9 +2,32 @@
 
 
 #### Add API Key to environment
-export OPENAI_API_KEY='api_key'
+- export OPENAI_API_KEY='openai api_key'
+- export XI_API_KEY='genesys api_key'
 
-### Usage
+## OpenAI Text Response Generator
+
+Several responses are available that cover the various conversation states
+### character_dialog usage
 get_question_response("arnold shwartzenegger", "homer simpson", "are you in movies?")
 
-response: D'oh! Yeah, you might have seen my mug on the big screen now and then. I do a little acting here and there, nothing too fancy!
+- returns character response:  e.g. D'oh! Yeah, you might have seen my mug on the big screen now and then. I do a little acting here and there, nothing too fancy!
+
+
+## ElevenLabs text-to-speech
+### audio generator usage
+
+get_response_audio("arnold", "Who is your daddy and what does he do?")
+- returns byte[] of mu-law PCM encoded audio
+
+### Available characters
+- arnold
+- elmer
+- piggy
+- scarlett
+- crash
+- yoda
+- freeman
+- mario
+- hussain
+- homer
---
+++ audio_generator.py
@@ -0,0 +1,59 @@
+import requests
+import os
+
+CHUNK_SIZE = 1024  # Size of chunks to read/write at a time
+XI_API_KEY = os.environ["XI_API_KEY"]  # Your API key for authentication
+
+voices = {"arnold": "4q1HMIvKfgbjxb0BZsu3",
+          "elmer": "7BXee8r6HkvGL1OZIoEb",
+          "piggy": "I6hJhvdUjFywlRS2sHr1",
+          "scarlett": "LJwyTpb1P1Y0YgmwA1cU",
+          "crash": "O2Jg7hvavg2CM92Adt5T",
+          "yoda": "OPPPrIAzqxpRo0z4bVUt",
+          "freeman": "cKjPC0QFrO0D9852wMzs",
+          "mario": "lXteNbvxCHpyDoNeWC0b",
+          "hussain": "ul8JnhojCgF8iA2WgCjz",
+          "homer": "vWgQedHHDqGUvqr7A08O"}
+
+
+def get_response_audio(voice_character, response_text):
+    voice_id = voices[voice_character]
+    # Construct the URL for the Text-to-Speech API request
+    tts_url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}/stream"
+
+    # Set up headers for the API request, including the API key for authentication
+    headers = {
+        "Accept": "application/json",
+        "xi-api-key": XI_API_KEY
+    }
+
+    # Set up the data payload for the API request, including the text and voice settings
+    data = {
+        "text": response_text,
+        "model_id": "eleven_multilingual_v2",
+        "voice_settings": {
+            "stability": 0.5,
+            "similarity_boost": 0.8,
+            "style": 0.0,
+            "use_speaker_boost": True
+        }
+    }
+
+    # set output format to be mu-law PCM for Genesys AudioHook support
+    querystring = {"output_format": "ulaw_8000"}
+
+    # Make the POST request to the TTS API with headers and data, enabling streaming response
+    response = requests.post(tts_url, headers=headers, json=data, stream=True, params=querystring)
+
+    # Check if the request was successful
+    if response.ok:
+        byte_array = bytearray()
+        # Read the response in chunks and write to the file
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            if chunk:  # Filter out keep-alive new chunks
+                byte_array.extend(chunk)
+
+        return byte_array
+    else:
+        # Print the error message if the request was not successful
+        print(response.text)
